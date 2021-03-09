4177d63f390c8726d3f9a6868fd4f7223cac94cd
JoeSahrmann <joexs10@gmail.com>
2021-03-09 12:25:32 -0600

    time to improve on the time

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,17 @@
 #FIZZbuzz by Joe Xavier
+#Keep track of time
+from datetime import datetime
+
+#Saves the time when it starts
+startTime = datetime.now()
+
 #Start Range loop
-for i in range(101):#Makes Range
-    if (i == 0):#detechs zero and replaces it with
+for i in range(101):
+    
+#remove the math and do it at the beging
+    
+#detechs zero and replaces it  
+    if (i == 0):                
         print("Welcome to FizzBuzz")
     elif (i % 3 == 0 and i % 5 == 0):
         print("FizzBuzz")
@@ -11,3 +21,7 @@ for i in range(101):#Makes Range
         print("Buzz")
     else:
         print(i)
+        
+#How long did it take
+print(datetime.now() - startTime)
+#original time before improvement 0:00:00.232463
