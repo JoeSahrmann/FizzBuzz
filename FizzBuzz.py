0ce2800b2ef91d24c534f5643d93c9aab616c6d4
JoeSahrmann <joexs10@gmail.com>
2021-03-09 12:30:43 -0600

    Made it more efficent

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,19 +5,24 @@ from datetime import datetime
 #Saves the time when it starts
 startTime = datetime.now()
 
+#Make range
+ranger = range(101)
+
 #Start Range loop
-for i in range(101):
+for i in ranger:
     
 #remove the math and do it at the beging
+    three = i % 3
+    five = i % 5
     
 #detechs zero and replaces it  
     if (i == 0):                
         print("Welcome to FizzBuzz")
-    elif (i % 3 == 0 and i % 5 == 0):
+    elif (three == 0 and five == 0):
         print("FizzBuzz")
-    elif (i % 3 == 0):
+    elif (three == 0):
         print("Fizz")
-    elif (i % 5 == 0):
+    elif (five == 0):
         print("Buzz")
     else:
         print(i)
@@ -25,3 +30,4 @@ for i in range(101):
 #How long did it take
 print(datetime.now() - startTime)
 #original time before improvement 0:00:00.232463
+#NEW time 0:00:00.226478
