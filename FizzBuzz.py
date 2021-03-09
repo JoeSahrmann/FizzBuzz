96ee2886002a1770994de829b1e2fc71d51bf62a
JoeSahrmann <joexs10@gmail.com>
2021-03-09 11:55:34 -0600

    first attempt

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,12 @@
 #FIZZbuzz by Joe Xavier
+#Make range here
+for i in range(101):
+    if (i % 3 == 0):
+        print("Fizz")
+    if (i % 5 == 0):
+        print("Buzz")
+    if (i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz")
+    if (i == 0):
+        print("Welcome to FizzBuzz")
+    print(i)
